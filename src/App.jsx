4e197e73eab33858ecc6aa6ad6e22b43733338ae
Onkar Patel <onkarpatel10@gmail.com>
2025-06-05 18:59:02 +0530

    Global Styles Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles.js';
+import Button from './ui/Button.jsx';
+import Input from './ui/Input.jsx';
 
+// H1 styled component for the heading
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
 `;
 
+// Styled component for the main app container
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
   return (
-    <div>
-      <H1>Wild Oasis</H1>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Wild Oasis</H1>
+        <Button onClick={() => alert('Check In!!')}>Check In</Button>
+        <Button onClick={() => alert('Check Out!!')}>Check Out</Button>
+        <Input type='text' placeholder='Enter your name' />
+      </StyledApp>
+    </>
   );
 }
 
