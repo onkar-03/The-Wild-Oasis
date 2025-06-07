3311c8ac433cdcc4a7474b6501d2b09e5946b42c
Onkar Patel <onkarpatel10@gmail.com>
2025-06-08 01:14:39 +0530

    Added Update Settings Functionality

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -3,6 +3,8 @@ import FormRow from '../../ui/FormRow.jsx';
 import Input from '../../ui/Input.jsx';
 import { useSettings } from './useSettings.js';
 import Spinner from '../../ui/Spinner.jsx';
+import { useUpdateSetting } from './useUpdateSetting.js';
+import { updateSetting } from '../../services/apiSettings.js';
 
 function UpdateSettingsForm() {
   const {
@@ -15,22 +17,49 @@ function UpdateSettingsForm() {
     } = {},
   } = useSettings();
 
+  const { isUpdating, updateSrettings } = useUpdateSetting();
+
   if (isLoading) {
     <Spinner />;
   }
+
+  function handleUpdate(e, field) {
+    const { value } = e.target;
+    // console.log(value);
+
+    // If the value is empty, we don't want to update the setting
+    if (!value) return;
+
+    updateSetting({ [field]: value });
+  }
+
   return (
     <Form>
       <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' defaultValue={minBookingLength} />
+        <Input
+          type='number'
+          id='min-nights'
+          defaultValue={minBookingLength}
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, 'minBookingLength')}
+        />
       </FormRow>
       <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' defaultValue={maxBookingLength} />
+        <Input
+          type='number'
+          id='max-nights'
+          defaultValue={maxBookingLength}
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, 'maxBookingLength')}
+        />
       </FormRow>
       <FormRow label='Maximum guests/booking'>
         <Input
           type='number'
           id='max-guests'
           defaultValue={maxGuestsPerBooking}
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, 'maxGuestsPerBooking')}
         />
       </FormRow>
       <FormRow label='Breakfast price'>
@@ -38,6 +67,8 @@ function UpdateSettingsForm() {
           type='number'
           id='breakfast-price'
           defaultValue={breakfastPrice}
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, 'breakfastPrice')}
         />
       </FormRow>
     </Form>
