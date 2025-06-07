d4df7ac7eac3a1e10b8f8fcb46fe189af2b70c25
Onkar Patel <onkarpatel10@gmail.com>
2025-06-08 00:59:08 +0530

    Fetching Settings from Supabase

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,21 +1,44 @@
-import Form from '../../ui/Form';
-import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+import Form from '../../ui/Form.jsx';
+import FormRow from '../../ui/FormRow.jsx';
+import Input from '../../ui/Input.jsx';
+import { useSettings } from './useSettings.js';
+import Spinner from '../../ui/Spinner.jsx';
 
 function UpdateSettingsForm() {
+  const {
+    isLoading,
+    settings: {
+      minBookingLength,
+      maxBookingLength,
+      maxGuestsPerBooking,
+      breakfastPrice,
+    } = {},
+  } = useSettings();
+
+  if (isLoading) {
+    <Spinner />;
+  }
   return (
     <Form>
       <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+        <Input type='number' id='min-nights' defaultValue={minBookingLength} />
       </FormRow>
       <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+        <Input type='number' id='max-nights' defaultValue={maxBookingLength} />
       </FormRow>
       <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+        <Input
+          type='number'
+          id='max-guests'
+          defaultValue={maxGuestsPerBooking}
+        />
       </FormRow>
       <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+        <Input
+          type='number'
+          id='breakfast-price'
+          defaultValue={breakfastPrice}
+        />
       </FormRow>
     </Form>
   );
