b9ce672eaae5091999072ced1975704cc46b93c9
Onkar Patel <onkarpatel10@gmail.com>
2025-06-09 02:59:45 +0530

    Added Update User Data Feature

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,19 +1,21 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+import UpdateUserDataForm from '../features/authentication/UpdateUserDataForm.jsx';
+import UpdatePasswordForm from '../features/authentication/UpdatePasswordForm.jsx';
+import Heading from '../ui/Heading.jsx';
+import Row from '../ui/Row.jsx';
 
 function Account() {
   return (
     <>
-      <Heading as="h1">Update your account</Heading>
+      <Heading as='h1'>Update your account</Heading>
 
       <Row>
-        <Heading as="h3">Update user data</Heading>
-        <p>Update user data form</p>
+        <Heading as='h3'>Update user data</Heading>
+        <UpdateUserDataForm />
       </Row>
 
       <Row>
-        <Heading as="h3">Update password</Heading>
-        <p>Update user password form</p>
+        <Heading as='h3'>Update password</Heading>
+        <UpdatePasswordForm />
       </Row>
     </>
   );
